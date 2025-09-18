637d6f563f4cc2561d743bdfbf698cc0c606d91a
Md Uddin <uddinshams74@gmail.com>
2025-09-18 15:27:32 -0400

    Minor update 3

--- src/services/api.js
+++ src/services/api.js
@@ -13,32 +13,38 @@ const api = axios.create({
 export const filmsAPI = {
   getTopRented: () => api.get('/films/top-rented'),
   getFilmById: (id) => api.get(`/films/${id}`),
-  searchFilms: (query) => api.get(`/films/search?q=${encodeURIComponent(query)}`),
-  rentFilm: (filmId, customerId) => api.post(`/films/${filmId}/rent`, { customer_id: customerId }),
+  searchFilms: (filters) => {
+    const params = new URLSearchParams();
+    if (filters.title) params.append('title', filters.title);
+    if (filters.actorName) params.append('actorName', filters.actorName);
+    if (filters.genre) params.append('genre', filters.genre);
+    return api.get(`/films/search?${params.toString()}`);
+  },
+  getFilms: (page = 1, limit = 20) => api.get(`/films?page=${page}&limit=${limit}`),
+  rentFilm: (filmId, customerId, storeId) => api.post(`/films/${filmId}/rent`, { customer_id: customerId, store_id: storeId }),
 };
 
 // Actors API
 export const actorsAPI = {
   getTopActors: () => api.get('/actors/top'),
   getActorById: (id) => api.get(`/actors/${id}`),
+  getActorTopRentedFilms: (id) => api.get(`/actors/${id}/top-rented-films`),
+  searchActors: (query) => api.get(`/actors/search/${query}`),
 };
 
 // Customers API
 export const customersAPI = {
-  getCustomers: (page = 1, limit = 20) => {
-    const params = new URLSearchParams({ page, limit });
-    return api.get(`/customers?${params.toString()}`);
-  },
-  getCustomerById: (id) => api.get(`/customers/${id}`),
-  createCustomer: (customerData) => api.post('/customers', customerData),
-  updateCustomer: (id, customerData) => api.put(`/customers/${id}`, customerData),
-  deleteCustomer: (id) => api.delete(`/customers/${id}`),
+  getCustomers: (page = 1, limit = 20) => api.get(`/customers?page=${page}&limit=${limit}`),
   searchCustomers: (filters) => {
     const params = new URLSearchParams();
     if (filters.customerId) params.append('customerId', filters.customerId);
     if (filters.name) params.append('name', filters.name);
     return api.get(`/customers/search?${params.toString()}`);
   },
+  getCustomerById: (id) => api.get(`/customers/${id}`),
+  createCustomer: (customerData) => api.post('/customers', customerData),
+  updateCustomer: (id, customerData) => api.put(`/customers/${id}`, customerData),
+  deleteCustomer: (id) => api.delete(`/customers/${id}`),
   returnRental: (customerId, rentalId) => api.post(`/customers/${customerId}/return-rental`, { rental_id: rentalId }),
 };
 
