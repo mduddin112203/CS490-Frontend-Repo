1498f51706c53e39beaaa8b5bf8138bdafe756bb
Md Uddin <uddinshams74@gmail.com>
2025-09-18 11:43:56 -0400

    Feature 14/14 - As a user I want to be able to indicate that a customer has returned a rented movie. Complete frontend with navigation, routing, and all styling.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
 import LandingPage from './pages/LandingPage';
-import FilmDetails from './pages/FilmDetails';
-import ActorDetails from './pages/ActorDetails';
 import FilmsPage from './pages/FilmsPage';
 import CustomersPage from './pages/CustomersPage';
-import './App.css';
+import FilmDetails from './pages/FilmDetails';
+import ActorDetails from './pages/ActorDetails';
+import CustomerDetails from './pages/CustomerDetails';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/films" element={<FilmsPage />} />
-          <Route path="/films/:id" element={<FilmDetails />} />
-          <Route path="/actors/:id" element={<ActorDetails />} />
-          <Route path="/customers" element={<CustomersPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="navbar">
+          <div className="nav-container">
+            <Link to="/" className="nav-logo">
+              Sakila DVD Store
+            </Link>
+            <div className="nav-menu">
+              <Link to="/" className="nav-link">Home</Link>
+              <Link to="/films" className="nav-link">Films</Link>
+              <Link to="/customers" className="nav-link">Customers</Link>
+            </div>
+          </div>
+        </nav>
+
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/films" element={<FilmsPage />} />
+            <Route path="/films/:id" element={<FilmDetails />} />
+            <Route path="/actors/:id" element={<ActorDetails />} />
+            <Route path="/customers" element={<CustomersPage />} />
+            <Route path="/customers/:id" element={<CustomerDetails />} />
+          </Routes>
+        </main>
+
+        <footer className="footer">
+          <p>&copy; 2025 Sakila DVD Store - Md Uddin CS490</p>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
